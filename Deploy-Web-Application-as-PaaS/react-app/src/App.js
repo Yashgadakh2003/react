323967e51a6359bf6379e87c0875bf29ff454d41
Yashgadakh2003 <129044630+Yashgadakh2003@users.noreply.github.com>
2024-09-25 16:29:01 +0530

    Update App.js

--- Deploy-Web-Application-as-PaaS/react-app/src/App.js
+++ Deploy-Web-Application-as-PaaS/react-app/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={react} className="react image" alt="react" />
         <p>
            Welcome to my react Application
         </p>
